41bfcf954789d8bd530770d1638bf43f110c6746
PRASANJIT ROY <roy2402011@gmail.com>
2026-04-09 23:41:17 +0600

    by me

---
+++ A_Antimedian_Deletion.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+     if(n==1)
+        {
+            cout<<"1"<<endl;
+            return ;
+        } 
+
+    for(int i=0;i<n;i++)
+    {
+       
+      cout<<"2"<<" ";
+
+    }
+    cout<<endl;
+}
+int main() {
+    fast_io();
+    int t;
+    cin>>t;
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+
+// PRASANJIT ROY , CS learner
